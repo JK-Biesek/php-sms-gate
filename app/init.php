77f9de7321f2e553c944e372493fad791c60898f
jakub <jakub.webdesign@gmail.com>
2018-03-03 19:59:09 +0100

    add bootstrap

--- app/init.php
+++ app/init.php
@@ -1,5 +1,6 @@
 <?php
   session_start();
+  ob_start();
   require '../vendor/autoload.php';
 
   $config = require 'config.php';
---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<?php
+require '../app/init.php';
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>SMS</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+     <link rel="stylesheet" href="/css/master.css">
+   </head>
+   <body>
+     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+       <div class="container">
+         <div class="navbar-header">
+           <button type="button" name="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+             <span class="sr-only">Toggle Navigation</span>
+             <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+           </button>
+           <a class="navbar-brand" href="#">SMS Appication</a>
+         </div>
+        
+         </div>
+       </div>
+     </nav>
+   </body>
+ </html>
